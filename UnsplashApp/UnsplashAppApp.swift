71a810de0097b4d0af7442a00a4fe72c6b869253
Tristan <tristan.ginet@etu.univ-smb.fr>
2024-01-23 13:46:45 +0100

    Initial Commit

---
+++ UnsplashApp/UnsplashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAppApp.swift
+//  UnsplashApp
+//
+//  Created by Tristan GINET on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
